8a4753cd5d47b7e9f056c2b1243dd117bd39fae7
Erez <erezfree29@gmail.com>
2021-05-21 13:34:46 -0700

    edit rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
   def nav_bar
     content_tag(:div, class: 'nav_bar') do
-        content_tag(:div, (link_to 'Home', root_path, class: 'link')) +
-          content_tag(:div, (link_to 'Sign up', new_user_registration_path, class: 'link' if !user_signed_in? &&
-         (controller.controller_name != 'registrations'))) +
-          content_tag(:div, (link_to 'Sign_in', user_session_path, class: 'link' if !user_signed_in? &&
-          (controller.controller_name != 'sessions'))) +
-          content_tag(:div,
-                      (if user_signed_in?
-                         link_to 'Sign out', destroy_user_session_path, class: 'link',
-                                                                        method: :delete
-                       end))
-      end
+      content_tag(:div, (link_to 'Home', root_path, class: 'link')) +
+        content_tag(:div, (link_to 'Sign up', new_user_registration_path, class: 'link' if !user_signed_in? &&
+       (controller.controller_name != 'registrations'))) +
+        content_tag(:div, (link_to 'Sign_in', user_session_path, class: 'link' if !user_signed_in? &&
+        (controller.controller_name != 'sessions'))) +
+        content_tag(:div,
+                    (if user_signed_in?
+                       link_to 'Sign out', destroy_user_session_path, class: 'link',
+                                                                      method: :delete
+                     end))
+    end
   end
 end
