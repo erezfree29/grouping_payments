d89baab7f78e8d3387b252476db29123d9bb744d
Erez <erezfree29@gmail.com>
2021-05-27 10:20:12 -0700

    edit navbar in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def nav_bar
     content_tag(:div, class: 'nav_bar') do
+      content_tag(:div, class: 'links') do
       content_tag(:div, (link_to 'Home', root_path, class: 'link')) +
         content_tag(:div, (link_to 'Sign up', new_user_registration_path, class: 'link' if !user_signed_in? &&
        (controller.controller_name != 'registrations'))) +
@@ -11,8 +12,29 @@ module ApplicationHelper
                        link_to 'Sign out', destroy_user_session_path, class: 'link',
                                                                       method: :delete
                      end))
+       end
     end
   end
+
+  def battery
+    content_tag(:div, class: 'icons') do
+      content_tag(:i, nil,class: 'fas fa-battery-half')
+     end
+  end
+
+  def circle
+    content_tag(:div, class: 'icons') do
+      content_tag(:i, nil,class: 'fas fa-circle')
+     end
+  end
+
+  def transparent_circle
+    content_tag(:div, class: 'icons') do
+      content_tag(:i, nil,class: 'far fa-circle')
+     end
+  end
+
+
 
   def bootstrap_class_for(flash_type)
     { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning',
