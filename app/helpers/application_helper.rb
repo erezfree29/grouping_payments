ab6b89aca33596955acbcd3d8fc3052e1c0d8d85
Erez <erezfree29@gmail.com>
2021-05-27 11:19:50 -0700

    fix linters errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,39 +2,37 @@ module ApplicationHelper
   def nav_bar
     content_tag(:div, class: 'nav_bar') do
       content_tag(:div, class: 'links') do
-      content_tag(:div, (link_to 'Home', root_path, class: 'link')) +
-        content_tag(:div, (link_to 'Sign up', new_user_registration_path, class: 'link' if !user_signed_in? &&
-       (controller.controller_name != 'registrations'))) +
-        content_tag(:div, (link_to 'Sign_in', user_session_path, class: 'link' if !user_signed_in? &&
-        (controller.controller_name != 'sessions'))) +
-        content_tag(:div,
-                    (if user_signed_in?
-                       link_to 'Sign out', destroy_user_session_path, class: 'link',
-                                                                      method: :delete
-                     end))
-       end
+        content_tag(:div, (link_to 'Home', root_path, class: 'link')) +
+          content_tag(:div, (link_to 'Sign up', new_user_registration_path, class: 'link' if !user_signed_in? &&
+         (controller.controller_name != 'registrations'))) +
+          content_tag(:div, (link_to 'Sign_in', user_session_path, class: 'link' if !user_signed_in? &&
+          (controller.controller_name != 'sessions'))) +
+          content_tag(:div,
+                      (if user_signed_in?
+                         link_to 'Sign out', destroy_user_session_path, class: 'link',
+                                                                        method: :delete
+                       end))
+      end
     end
   end
 
   def battery
     content_tag(:div, class: 'icons') do
-      content_tag(:i, nil,class: 'fas fa-battery-half')
-     end
+      content_tag(:i, nil, class: 'fas fa-battery-half')
+    end
   end
 
   def circle
     content_tag(:div, class: 'icons') do
-      content_tag(:i, nil,class: 'fas fa-circle')
-     end
+      content_tag(:i, nil, class: 'fas fa-circle')
+    end
   end
 
   def transparent_circle
     content_tag(:div, class: 'icons') do
-      content_tag(:i, nil,class: 'far fa-circle')
-     end
+      content_tag(:i, nil, class: 'far fa-circle')
+    end
   end
-
-
 
   def bootstrap_class_for(flash_type)
     { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning',
